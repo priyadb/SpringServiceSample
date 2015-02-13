f11bc085509aa8dfdee7c9e85569f8bdd8ad6879
Priyadarshini Balachandran <dharshus.1989@gmail.com>
2015-02-13 01:11:40 -0600

    SpringServiceSample
    Spring Service Example

---
+++ SpringServiceSample/src/com/programmingfree/springservice/controller/SpringServiceController.java
@@ -0,0 +1,16 @@
+package com.programmingfree.springservice.controller;
+
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/service/greeting")
+public class SpringServiceController {
+	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
+	public String getGreeting(@PathVariable String name) {
+		String result="Hello "+name;		
+		return result;
+	}
+}
